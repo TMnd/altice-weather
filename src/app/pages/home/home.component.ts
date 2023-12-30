2863a04f77e09617b43af01106e7cec28efe25b3
João Luís Amaral <amaral.joao@ua.pt>
2023-12-30 01:31:54 +0000

    Fix spinner and added todo list for the rest (FIX MOBILE)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { InputDataComponent } from '../../components/input-data/input-data.component';
 import { DataTableComponent } from '../../components/data-table/data-table.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
+import { NotesComponent } from '../../components/notes/notes.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InputDataComponent, DataTableComponent, FooterComponent],
+  imports: [InputDataComponent, DataTableComponent, NotesComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
