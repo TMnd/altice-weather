fedadf49d1187fedee1500f8c706599eba7a2642
João Amaral <amaral.joao@ua.pt>
2024-01-03 15:50:36 +0000

    Prod enviroment add https to openweathermap

--- src/app/enviroments/enviroment.prod.ts
+++ src/app/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    weatherApi: 'http://api.openweathermap.org',
+    weatherApi: 'https://api.openweathermap.org',
     weatherApiKey: '14d15b894d9087d2e826fa3cc192a390',
     crudcrudUrl: 'https://crudcrud.com/api/',
     crudcrudApiKey: '924be92fdb3c4fdf870706dcad98c43e'
