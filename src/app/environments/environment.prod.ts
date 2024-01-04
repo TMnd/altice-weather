71529e1abe1fcf9adba22968ab3798b2bb146d7e
João Amaral <amaral.joao@ua.pt>
2024-01-04 00:15:19 +0000

    Update crudcrud api

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     weatherApi: 'https://api.openweathermap.org',
     weatherApiKey: '14d15b894d9087d2e826fa3cc192a390',
     crudcrudUrl: 'https://crudcrud.com/api/',
-    crudcrudApiKey: '924be92fdb3c4fdf870706dcad98c43e'
+    crudcrudApiKey: 'e21a024798fb44ca8489b3d88a489811'
 };  
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     weatherApi: 'http://api.openweathermap.org',
     weatherApiKey: '14d15b894d9087d2e826fa3cc192a390',
     crudcrudUrl: 'https://crudcrud.com/api/',
-    crudcrudApiKey: '924be92fdb3c4fdf870706dcad98c43e'
+    crudcrudApiKey: 'e21a024798fb44ca8489b3d88a489811'
 };
